3f772bb58542256d449aebacad6569b9b1ef01dd
Het Savani <114601993+Hetsavani@users.noreply.github.com>
2024-03-28 09:51:51 +0530

    Add files via upload

---
+++ Auth_schema.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        username: {
+            type: String,
+            required: true,
+            unique: true
+        }
+    }
+);
+module.exports = mongoose.model('Users',schema);
